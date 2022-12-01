143f73a85e5974e9849d0d1f75842982b733fa79
21musie <104737346+21musie@users.noreply.github.com>
2022-12-01 22:22:38 +0300

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    sum = 0
-    if (len(sys.argv) > 1):
-        for i in range(len(sys.argv) - 1):
-            sum = sum + int(sys.argv[i + 1])
-    print("{:d}".format(sum))
