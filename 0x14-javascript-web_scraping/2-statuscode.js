5346023688eeb54c563fb9f0e461d0fbd4a2eb0d
21musie <104737346+21musie@users.noreply.github.com>
2023-04-27 02:37:59 +0300

    0x14

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
+
 const request = require('request');
-request(process.argv[2], function (error, response) {
-  if (error == null) {
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
     console.log('code: ' + response.statusCode);
   }
 });
