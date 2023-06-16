859be8973a8b655b0aab62e20100e4be8e37c375
21musie <104737346+21musie@users.noreply.github.com>
2023-06-16 23:09:59 +0300

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(list=[]):
     list += [1]
-    return(("Holberton, " * (len(list) - 1)) + "Holberton")
+    return(("BestSchool, " * (len(list) - 1)) + "BestSchool")
