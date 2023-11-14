9e1b92f9a9234a907eb4a326e6e603642f31f905
Musie <mmengesha27@gmail.com>
2023-11-15 02:11:22 +0300

    number 9

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-let start = 0;
-let end = list.length - 1;
-while (start < end) {
-temp = list[start];
-list[start] = list[end];
-list[end] = temp;
+  let start = 0;
+  let end = list.length - 1;
+  let temp = 0;
+  while (start < end) {
+    temp = list[start];
+    list[start] = list[end];
+    list[end] = temp;
 
-start++;
-end--;
-}
-return list;
-}
+    start++;
+    end--;
+  }
+  return list;
+};
